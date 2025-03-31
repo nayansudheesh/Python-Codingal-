ec9403549487a598cadd878102973015f70a7ddc
nayansudheesh <nayanthejassudheesh@gmail>
2025-03-31 19:21:20 +0530

    Datatypes

--- Lesson 3/Datatypes.py
+++ Lesson 3/Datatypes.py
@@ -1,1 +1,11 @@
-ifiegei
+a = 33
+print("the datatype of a is :" , type(a))
+
+b = 3.3
+print("the datatype of b is :" , type(b))
+
+c = "hi!"
+print("the datatype of c is :" , type(c))
+
+d = True
+print("the datatype of d is :" , type(d))
