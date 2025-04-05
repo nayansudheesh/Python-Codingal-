abe19d6d6218316fa7119fb3696618f66711a398
nayansudheesh <nayanthejassudheesh@gmail>
2025-04-05 14:51:58 +0530

    Percentage calculator

---
+++ Lesson 4/Percentage_calculator.py
@@ -0,0 +1,10 @@
+print("Marks obtained in 4 subjects")
+hindi = int(input("Enter marks for hindi") )
+math = int(input("Enter marks for math") )
+english = int(input("Enter marks for english") )
+computing = int(input("Enter marks for computing") )
+sum = hindi + math + english + computing
+print("Sum of all subjects:", sum)
+perc =(sum/500)*100
+print(end = "Percentage =")
+print(perc)
