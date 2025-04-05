76038dadad44444ead4136e1657e5ca9a6fcb98d
nayansudheesh <nayanthejassudheesh@gmail>
2025-04-05 14:32:44 +0530

    Average

---
+++ Lesson 4/Average.py
@@ -0,0 +1,15 @@
+#98 94 41 96 11
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+print("The height of tree1 is:", tree1, "Inches")
+print("The height of tree2 is:", tree2, "Inches")
+print("The height of tree3 is:", tree3, "Inches")
+print("The height of tree4 is:", tree4, "Inches")
+print("The height of tree5 is:", tree5, "Inches")
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("The sum of trees is:" , sum, "Inches")
+average = sum/5
+print("The average height is:", average, "Inches")
