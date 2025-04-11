005892e026d465ea067684b5604afa849f41efdf
nayansudheesh <nayanthejassudheesh@gmail>
2025-04-11 17:48:54 +0530

    Profit Loss

---
+++ Lesson 5/Profit-loss.py
@@ -0,0 +1,6 @@
+income = int(input("Enter Income"))
+cost = int(input("Enter cost"))
+if (income>cost) : profit = True
+print("Do you have profit?" , profit)
+total_profit =income-cost
+print("Total profit = " , total_profit)
