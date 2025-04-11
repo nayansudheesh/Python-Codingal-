e5fd581c258b77d0a6e43c5dd93eca3987899f99
nayansudheesh <nayanthejassudheesh@gmail>
2025-04-11 18:01:18 +0530

    > or <

---
+++ Greater - smaller.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter any number"))
+print("Num 1 = " , num1)
+if num1 > 15 : 
+    print("Num1 is greater than 15 (", num1 ," > 15)")
+    print("This is in if block")
+else:
+    print("Num1 is smaller than 15,(", num1 ,"< 15)")
+    print("This is in else block")
