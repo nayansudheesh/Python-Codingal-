03cbf311242104bc96f4962e6e7be65d2ddff977
nayansudheesh <nayanthejassudheesh@gmail>
2025-04-12 14:36:04 +0530

    !=

---
+++ Lesson 6/!=.py
@@ -0,0 +1,18 @@
+a = 10
+b = 12
+c = 12
+
+print( a != b)
+print( b != c)
+a = "python"
+b = "Coding"
+if a != b :
+ print(" Python and Coding is different")
+
+a = 4
+b = 5
+if ( a == 1)  != (b == 5):
+ print("hello")
+a = int(input("Enter any number"))
+if a%2 != 0 :
+ print(a , "Is not even") 
