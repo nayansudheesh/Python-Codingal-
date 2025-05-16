675aaab4a36cb049d43a064a9220d9dee5b7e9f2
nayansudheesh <nayanthejassudheesh@gmail>
2025-05-16 18:02:25 +0530

    Pass

---
+++ Lesson 14/Pass.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    if i%200 == 0:
+        print("Twist")
+    elif i % 15 == 0:
+        pass
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 00:
+        print("Fizz")
+    else:
+        print(i)
