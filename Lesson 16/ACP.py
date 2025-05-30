b2f7e8b20b4053e618f624da714633c74c241617
nayansudheesh <nayanthejassudheesh@gmail>
2025-05-30 18:13:39 +0530

    ACP

---
+++ Lesson 16/ACP.py
@@ -0,0 +1,6 @@
+import math
+
+theta = int(input("Enter the number to find sin , cos, and tan(theeta)"))
+print("Sin of theta is" , math.sin(theta))
+print("cos of theta is" , math.cos(theta))
+print("tan of theta is" , math.tan(theta))
