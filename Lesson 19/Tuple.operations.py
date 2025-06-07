62e0e2b21beccb98e4ed3a77a459f9cfe8ae1cc6
nayansudheesh <nayanthejassudheesh@gmail>
2025-06-07 14:41:42 +0530

    Tuple in python

---
+++ Lesson 19/Tuple.operations.py
@@ -0,0 +1,14 @@
+tuplex = ('tuplex' , False , 3.2 , 0 , 1)
+print(tuplex)
+tuplex = (4 , 6 , 2 , 8 , 3 , 1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+#tuple1
+tuple1 = (50 , 60 , 10 , 70 , 40 , 50)
+print(tuple1.count(50))
+tuplex = ( 2 , 4 , 3 , 5 , 4 , 6 , 7 , 8 , 6 ,1 )
+slice = tuplex[3:5]
+print(slice)
+_slice = tuplex[:6]
+print(_slice)
