5c18806e0ee9a7d47827adf58e9cd03cc37b1e68
nayansudheesh <nayanthejassudheesh@gmail>
2025-06-13 18:16:55 +0530

    ACP

--- Lesson 20/ACP.py
+++ Lesson 20/ACP.py
@@ -1,7 +1,7 @@
 test_dict = { 'Age1': 6,'Age2': 12,'Age3': 8,'Age4': 6,'Age5': 12,}
 
 print("Original dictionary:" , test_dict)
-no = int(input("Which number to check frequency? (6 , 10 , 8 , 12)"))
+no = int(input("Which number to check frequency? (6 , 8 , 12)"))
 res = 0
 for key in test_dict:
     if test_dict[key] == no:
