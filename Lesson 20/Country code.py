b9f80dbc6ae59a8f3a1d673706bf5a8f428a518a
nayansudheesh <nayanthejassudheesh@gmail>
2025-06-13 18:09:44 +0530

    Country codes

---
+++ Lesson 20/Country code.py
@@ -0,0 +1,3 @@
+Country_codes = {'india': '0091', 'nepal': '00977', 'australia': '0025'}
+print("Country code for india:" , Country_codes.get('india' , 'Not found'))
+print("Country code for Japan:" , Country_codes.get('Japan' , 'Not found'))
