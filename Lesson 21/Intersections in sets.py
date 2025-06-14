9c79b7072b2151d7a4edee7b83f10191b9f10894
nayansudheesh <nayanthejassudheesh@gmail>
2025-06-14 14:41:15 +0530

    Intersection of sets

---
+++ Lesson 21/Intersections in sets.py
@@ -0,0 +1,6 @@
+set_a = {'blue' , 'yellow'}
+set_b = {'green' , 'blue'}#original sets
+print("Original set a: ", set_a)
+print("Original set b: ", set_b)
+set_c = set_a.intersection(set_b)#intersection
+print("Intersection of two sets( a , b) is:" , set_c)#output
