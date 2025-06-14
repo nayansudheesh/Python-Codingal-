f82815e14f45b01efb69181100ccb4d75aadee4d
nayansudheesh <nayanthejassudheesh@gmail>
2025-06-14 14:53:59 +0530

    ACP

---
+++ Lesson 21/ACP.py
@@ -0,0 +1,6 @@
+set1 = { "Yellow" , "Blue" , "Green" , "Red" , "Brown"}
+print("Set 1 originally is:" , set1)
+set2 = {"Yellow" , "Purple" , "Pink" , "Red" , "Green"}
+print("Set 2 originally is:" , set2)
+set3 = set1.symmetric_difference(set2)
+print("Symteric difference of set 1 and set 2 is:" , set3)
