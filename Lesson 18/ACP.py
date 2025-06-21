b53e80d78b287781e20c814e40bcae0c26308f11
nayansudheesh <nayanthejassudheesh@gmail>
2025-06-21 20:45:09 +0530

    Square it!

---
+++ Lesson 18/ACP.py
@@ -0,0 +1,8 @@
+#project , square it out! lesson 120.
+num = [3 , 4 ,5 , 6]
+n = 0
+for i in range (4):
+    square = num[n] * num[n]
+    print("square of" , num[n] , "Is " , square)
+    n = n + 1
+  
